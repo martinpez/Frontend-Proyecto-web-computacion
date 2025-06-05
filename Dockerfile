efb48e8e1975762cf15690df620515b3aeaa81b4
Martin <perezmercadomartin@gmail.com>
2025-06-05 17:11:24 -0500

    feat: add astrojs node and tailwind

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
-FROM node:18-alpine as builder
+FROM node:18-alpine
+
 WORKDIR /app
+
 COPY package.json yarn.lock ./
 RUN yarn install
+
 COPY . .
 RUN yarn build
 
-FROM nginx:alpine
-COPY --from=builder /app/dist /usr/share/nginx/html
-EXPOSE 80
+EXPOSE 3000
+
+CMD ["node", "./dist/server/entry.mjs"]
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
 // @ts-check
-import { defineConfig , envField } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-
+import { defineConfig, envField } from 'astro/config';
+import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  
-  },
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+
+  integrations: [tailwind()],
 
   env: {
-      schema:{
-        URL_API: envField.string({context:'server',access: 'public'})
-    }
-  
-  }
+    schema: {
+      URL_API: envField.string({ context: 'server', access: 'public' }),
+    },
+  },
 });
