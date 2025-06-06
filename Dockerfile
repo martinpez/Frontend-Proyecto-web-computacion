7b8920f5ab0c90c7874c5882475cfeeb46215b2c
Martin <perezmercadomartin@gmail.com>
2025-06-06 10:38:59 -0500

    Revert changes

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,9 @@ COPY package.json yarn.lock ./
 RUN yarn install
 
 COPY . .
-RUN yarn build
 
 # Expone el puerto usado por astro preview
-EXPOSE 3000
+EXPOSE 4321
 
 # Comando para iniciar el servidor de previsualización
 CMD ["yarn", "preview"]
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
-import node from '@astrojs/node';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig , envField } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
-
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  
+  },
 
   env: {
-    schema: {
-      URL_API: envField.string({ context: 'server', access: 'public' }),
-    },
-  },
+      schema:{
+        URL_API: envField.string({context:'server',access: 'public'})
+    }
+  
+  }
 });
